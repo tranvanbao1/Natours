10461cbe0743463ed44260392fb54474d725daf7
Brady Tran <baotrank22tpm4@gmail.com>
2020-07-08 10:19:22 +0700

    delete function

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
-app.patch('/api/v1/tours/:id', (req, res) => {
+app.delete('/api/v1/tours/:id', (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -65,11 +65,9 @@ app.patch('/api/v1/tours/:id', (req, res) => {
     });
   }
 
-  res.status(200).json({
+  res.status(204).json({
     status: 'success',
-    data: {
-      tour: '<Update tour here...>',
-    },
+    data: null
   });
 });
 const port = 3000;
